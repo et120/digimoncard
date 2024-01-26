b1862788b224d96ef14afc2d90ae6333361433f2
et120 <e.trotter120@gmail.com>
2024-01-26 09:56:18 -0800

    friday lecture

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,27 @@
-const DigimonApi = async () => {
-    const promise = await fetch("https://digimon-api.vercel.app/api/digimon");
+// IDs
+let digimonImg = document.getElementById("digimonImg");
+let digimonName = document.getElementById("digimonName");
+let digimonStatus = document.getElementById("digimonStatus");
+let favoriteBtn = document.getElementById("favoriteBtn");
+let digimonInput = document.getElementById("digimonInput");
+
+
+const DigimonApi = async (digimon) => {
+    const promise = await fetch(`https://digimon-api.vercel.app/api/digimon/name/${digimon}`);
     const data = await promise.json();
-    console.log(data);
+    // console.log(data);
+    return data;
 }
 
-DigimonApi();
+// DigimonApi("agumon");
+
+digimonInput.addEventListener('keydown', async (event) => {
+    //On enter I want this function to run
+    if(event.key === "Enter"){
+        let digimon = await DigimonApi(event.target.value);
+        console.log(digimon);
+        digimonImg.src = digimon[0].img;
+        digimonName.textContent = digimon[0].name;
+        digimonStatus.textContent = digimon[0].level;
+    }
+});
