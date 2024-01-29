a11237b731c4ccc370d18086953325da42a798b0
et120 <e.trotter120@gmail.com>
2024-01-29 10:18:47 -0800

    monday lecture walkthrough

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+import { saveToLocalStorage, getLocalStorage, removeFromLocalStorage } from "./localstorage.js";
+
 // IDs
 let digimonImg = document.getElementById("digimonImg");
 let digimonName = document.getElementById("digimonName");
@@ -5,6 +7,7 @@ let digimonStatus = document.getElementById("digimonStatus");
 let favoriteBtn = document.getElementById("favoriteBtn");
 let digimonInput = document.getElementById("digimonInput");
 
+let digimon = "";
 
 const DigimonApi = async (digimon) => {
     const promise = await fetch(`https://digimon-api.vercel.app/api/digimon/name/${digimon}`);
@@ -18,10 +21,14 @@ const DigimonApi = async (digimon) => {
 digimonInput.addEventListener('keydown', async (event) => {
     //On enter I want this function to run
     if(event.key === "Enter"){
-        let digimon = await DigimonApi(event.target.value);
+        digimon = await DigimonApi(event.target.value);
         console.log(digimon);
         digimonImg.src = digimon[0].img;
         digimonName.textContent = digimon[0].name;
         digimonStatus.textContent = digimon[0].level;
     }
+});
+
+favoriteBtn.addEventListener('click', () => {
+    saveToLocalStorage(digimon[0].name);
 });
