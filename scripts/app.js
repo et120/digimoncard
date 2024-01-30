af1f3e36c0e41447b93d5449648c1af7f0e2b759
et120 <e.trotter120@gmail.com>
2024-01-30 10:07:07 -0800

    tuesday tutorial walkthrough

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,8 @@ let digimonName = document.getElementById("digimonName");
 let digimonStatus = document.getElementById("digimonStatus");
 let favoriteBtn = document.getElementById("favoriteBtn");
 let digimonInput = document.getElementById("digimonInput");
+let getFavoritesBtn = document.getElementById("getFavoritesBtn");
+let getFavoritesDiv = document.getElementById("getFavoritesDiv");
 
 let digimon = "";
 
@@ -20,7 +22,7 @@ const DigimonApi = async (digimon) => {
 
 digimonInput.addEventListener('keydown', async (event) => {
     //On enter I want this function to run
-    if(event.key === "Enter"){
+    if (event.key === "Enter") {
         digimon = await DigimonApi(event.target.value);
         console.log(digimon);
         digimonImg.src = digimon[0].img;
@@ -31,4 +33,56 @@ digimonInput.addEventListener('keydown', async (event) => {
 
 favoriteBtn.addEventListener('click', () => {
     saveToLocalStorage(digimon[0].name);
+});
+
+getFavoritesBtn.addEventListener('click', () => {
+    // this retrieves our data from local storage and stores it into favorites variable
+    let favorites = getLocalStorage();
+
+    // clears div so the array displayed will not constantly repeat
+    getFavoritesDiv.textContent = "";
+
+    // map through each element in our array
+    favorites.map(digiName => {
+        // creating a p tag dynamically
+        let p = document.createElement("p"); // for every digimon in this array, we create a p tag
+
+        // setting text content to digiName
+        p.textContent = digiName;
+
+        // className replaces all classes with our new classes
+        p.className = "text-lg font-medium text-gray-500 dark:text-white"
+
+        // creating button dynamically
+        let button = document.createElement("button");
+
+        button.type = "button";
+        button.textContent = "X";
+        // classList allows us to be a little more concise, it doesnt replace all classes
+        button.classList.add(
+            "text-gray-400",
+            "bg-transparent",
+            "hover:bg-gray-200",
+            "hover:text-gray-900",
+            "rounded-lg",
+            "text-sm",
+            "w-8",
+            "h-8",
+            "justify-end",
+            "dark:hover:bg-gray-600",
+            "dark:hover:text-white"
+        );
+        
+        // creating addeventlistener for button to remove digiName from favorites when delete is clicked
+        button.addEventListener('click', () => {
+            removeFromLocalStorage(digiName);
+            p.remove();
+        });
+
+        // appending button to p tag
+        p.append(button);
+        
+        // appending p tag to div
+        getFavoritesDiv.append(p);
+    });
 });
